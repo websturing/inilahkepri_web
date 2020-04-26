5c0da410d8faaa4d7dd12495d23f29a0a1add6d3
websturing <websturing.project@gmail.com>
2020-04-26 16:36:26 +0700

    tempalte headline complate

--- public/js/app.js
+++ public/js/app.js
@@ -52845,15 +52845,15 @@ __webpack_require__.r(__webpack_exports__);
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.js");
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(vue__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var vue_router__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! vue-router */ "./node_modules/vue-router/dist/vue-router.esm.js");
+/* harmony import */ var vue_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! vue-router */ "./node_modules/vue-router/dist/vue-router.esm.js");
 /* harmony import */ var _js_url__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @/js/url */ "./resources/js/url.js");
 /* harmony import */ var _js_components_dashboard__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @/js/components/dashboard */ "./resources/js/components/dashboard.vue");
 
 
 
 
-vue__WEBPACK_IMPORTED_MODULE_0___default.a.use(vue_router__WEBPACK_IMPORTED_MODULE_4__["default"]);
-var router = new vue_router__WEBPACK_IMPORTED_MODULE_4__["default"]({
+vue__WEBPACK_IMPORTED_MODULE_0___default.a.use(vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]);
+var router = new vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]({
   mode: 'history',
   base: _js_url__WEBPACK_IMPORTED_MODULE_2__["default"].urlBase,
   hash: false,
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,93 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Inilahkperi.id - Disini Kami Berbagi</title>
+        <link rel="icon" href="{{url('public/images/fav.png')}}" type="image/png" sizes="16x16">
+        <link rel="stylesheet" href="{{url('public/pure/css/build/pure-min.css')}}">
+        <link rel="stylesheet" href="{{url('public/pure/css/build/grids-responsive-old-ie-min.css')}}">
+        <link rel="stylesheet" href="{{url('public/pure/css/build/grids-responsive-min.css')}}">
+        <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+            <link rel="stylesheet" href="{{url('public/pure/css/layouts/marketing-old-ie.css')}}">
+            <link rel="stylesheet" href="{{url('public/pure/css/layouts/marketing.css')}}">
+            <link rel="stylesheet" href="{{url('public/pure/css/layouts/style.css')}}">
 
     </head>
     <body>
+        
+       
+        <div class="header">
+            <div class="pure-menu pure-menu-horizontal pure-menu-fixed ikMenu">
+                <div class="ik-container">
+                    <div class="headlineLeft">
+                        <a class="pure-menu-heading" href="">
+                            <img src="{{url('public/images/logo1.png')}}" class="headerLogo">
+                        </a>
+                    </div>
+                    <div class="headlineRight">
+                    <ul class="">
+                        <li class="pure-menu-item pure-menu-selected"><a href="#" class="pure-menu-link">Beranda</a></li>
+                        <li class="pure-menu-item"><a href="#" class="pure-menu-link">Inilah Channel</a></li>
+                        <li class="pure-menu-item"><a href="#" class="pure-menu-link">Inilah News</a></li>
+                        <li class="pure-menu-item"><a href="#" class="pure-menu-link">Inilah Parlemen</a></li>
+                    </ul>
+                    </div>
+                    <div class="clearer"></div>
+                </div>
+            </div>
+        </div>
+        <br/><br/><br/><br/>
+        <section class="headline-content ik-sideStrip">
+            <div class="ik-container">
+                <div class="headlineLeft">
+                    <div class="padd-10">
+                        <span class="ik-date-p">22 April 2020</span>
+                        <h1>Paket Sembako Murah Untuk 
+                            Masyarakat Tanjungpinang 
+                            Terdampak Wabah Corona Diluncurkan
+                        </h1>
+                        <p>
+                            INILAHKEPRI, TANJUNGPINANG - Pemerintah Kota Tanjungpinang meluncurkan paket sembako
+                            murah untuk masyarakat yang terdampak  wabah pandemi virus Corona (Covid-19). 
+                            Paket sembako murah dibandrol dengan harga  Rp40 ribu, masyarakat Tanjungpinang dapat
+                            membawa pulang beras, gula pasir, tepung terigu, minyak goreng, mie instan dan telur.    
+                        <p>
+                        <span class="ik-date-p text-right">Baca Selengkapnya <i class="fa fa-arrow-right"></i></span>
+                    </div>
+                </div>
+                <div class="headlineRight">
+                    <div class="padd-10 ik-sideStrip-headline">
+                        <img src="{{url('public/images/head.jpg')}}" class="ik-headline-img">
+                    </div>
+                </div>
+                <div class="clearer"></div>
+
+
+                <div class="newTranding">
+                    <div class="panel">
+                        <img src="{{url('public/images/head.jpg')}}" class="ik-headline-img">
+                        <p>Paket Sembako Murah Untuk Masyarakat Tanjungpinang Terdampak Wabah Corona Diluncurkan</p>
+                        <span class="time">20 April 2020, 13 jam lalu</span>
+                    </div>
+                    <div class="panel">
+                        <img src="{{url('public/images/head.jpg')}}" class="ik-headline-img">
+                        <p>Paket Sembako Murah Untuk Masyarakat Tanjungpinang Terdampak Wabah Corona Diluncurkan</p>
+                        <span class="time">20 April 2020, 13 jam lalu</span>
+                    </div>
+                    <div class="panel">
+                        <img src="{{url('public/images/head.jpg')}}" class="ik-headline-img">
+                        <p>Paket Sembako Murah Untuk Masyarakat Tanjungpinang Terdampak Wabah Corona Diluncurkan</p>
+                        <span class="time">20 April 2020, 13 jam lalu</span>
+                    </div>
+                    <div class="panel">
+                        <img src="{{url('public/images/head.jpg')}}" class="ik-headline-img">
+                        <p>Paket Sembako Murah Untuk Masyarakat Tanjungpinang Terdampak Wabah Corona Diluncurkan</p>
+                        <span class="time">20 April 2020, 13 jam lalu</span>
+                    </div>
+                </div>
+                <div class="clearer"></div>
+            </div>
+        </section>
         <div id="app">
             <router-view :key="$route.fullPath"></router-view>
         </div>
