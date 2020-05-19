eee1e1ee449d6eee9d4a16e7220d3b82fd68e554
websturing <websturing.project@gmail.com>
2020-05-19 19:51:56 +0700

    local to online

--- resources/js/url.js
+++ resources/js/url.js
@@ -8,9 +8,11 @@ const iklan = "http://localhost/inilahkepri_web/public/iklan";
 // const urlBase = "inilahkepri_mobile";
 // const urlWeb = "http://192.168.100.8/inilahkepri_mobile";
 
-// const urlAxios = "http://tukangketik.my.id/inilahkepri_mobile/api";
-// const urlBase = "inilahkepri_mobile";
-// const urlWeb = "http://tukangketik.my.id/inilahkepri_mobile";
+const urlAxios = "http://tukangketik.my.id/inilahkepri_web/api";
+const urlBase = "inilahkepri_web";
+const urlWeb = "http://tukangketik.my.id/inilahkepri_web";
+const urlThumbnailBerita = "http://inilahkepri.id/resources/Artikel_Thumbnail";
+const iklan = "http://localhost/inilahkepri_web/public/iklan";
 
 
 export default {
