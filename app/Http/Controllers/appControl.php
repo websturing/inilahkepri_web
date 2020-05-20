ddb10c82ed1aa6a7f52e072481794137781b5eb3
websturing <websturing.project@gmail.com>
2020-05-20 11:59:44 +0700

    Redirect to mobile version revision

--- app/Http/Controllers/appControl.php
+++ app/Http/Controllers/appControl.php
@@ -12,7 +12,7 @@ class appControl extends Controller
         $agent = new Agent();
         
         if($agent->isMobile()){
-            return Redirect::to('http://www.tukanngketik.my.id/inilahkepri_mobile');
+            return Redirect::to('http://www.tukangketik.my.id/inilahkepri_mobile');
         }
         else{
             return view('welcome');
