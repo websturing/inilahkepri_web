47a1cc8515eba8d31a893879f9f8d9dd1985f4e6
websturing <websturing.project@gmail.com>
2020-11-01 21:08:19 +0700

    add iklan

--- resources/js/components/detail.vue
+++ resources/js/components/detail.vue
@@ -3,22 +3,32 @@
     <section class>
       <div class="ik-container">
         <div class="newsLeft">
-          <div v-for="(i,Iindex) in preiklanByAboveTitle" :key="Iindex">
+          <div v-for="(i, Iindex) in preiklanByAboveTitle" :key="Iindex">
             <img
-              :src="url.iklan+'/'+i.iklan_name"
-              style="width:100% !important; border-radius:0px; margin-bottom:10px"
+              :src="url.iklan + '/' + i.iklan_name"
+              style="
+                width: 100% !important;
+                border-radius: 0px;
+                margin-bottom: 10px;
+              "
             />
           </div>
-          <div class="judulDetail">{{berita.judul}}</div>
-          <div class="is-kategori">{{moment(berita.tgl_publish).format('l')}}</div>
-          <img :src="url.gambar+'/'+berita.Folder+'/'+berita.gambar" class="detailImage" />
-          <div class="imagecaption">{{berita.ket_gambar}}</div>
+          <div class="judulDetail">{{ berita.judul }}</div>
+          <div class="is-kategori">
+            {{ moment(berita.tgl_publish).format("l") }}
+          </div>
+          <img
+            :src="url.gambar + '/' + berita.Folder + '/' + berita.gambar"
+            class="detailImage"
+          />
+          <div class="imagecaption">{{ berita.ket_gambar }}</div>
           <p v-html="berita.isi_berita" class="isi_berita"></p>
+          <img :src="url.iklan + '/bannerCovid.gif'" style="width: 100%" />
         </div>
 
         <div class="newsRight">
-          <div v-for="(i,Iindex) in preiklanByRightContent" :key="Iindex">
-            <img :src="url.iklan+'/'+i.iklan_name" class />
+          <div v-for="(i, Iindex) in preiklanByRightContent" :key="Iindex">
+            <img :src="url.iklan + '/' + i.iklan_name" class />
           </div>
         </div>
         <div class="clearer"></div>
@@ -41,9 +51,9 @@ export default {
       iklan: [],
       url: {
         gambar: urlBase.urlThumbnailBerita,
-        iklan: urlBase.iklan
+        iklan: urlBase.iklan,
       },
-      filterCategory: "detail"
+      filterCategory: "detail",
     };
   },
   mounted() {
@@ -56,8 +66,8 @@ export default {
     preiklanByRightContent() {
       let iklan = this.iklan;
       if (this.filterCategory && this.filterCategory !== "all") {
-        iklan = iklan.filter(p => {
-          let foundCategory = p.iklan_position.findIndex(c => {
+        iklan = iklan.filter((p) => {
+          let foundCategory = p.iklan_position.findIndex((c) => {
             return (
               c.show_on === this.filterCategory &&
               c.position === "right_content"
@@ -71,8 +81,8 @@ export default {
     preiklanByAboveTitle() {
       let iklan = this.iklan;
       if (this.filterCategory && this.filterCategory !== "all") {
-        iklan = iklan.filter(p => {
-          let foundCategory = p.iklan_position.findIndex(c => {
+        iklan = iklan.filter((p) => {
+          let foundCategory = p.iklan_position.findIndex((c) => {
             return (
               c.show_on === this.filterCategory && c.position === "above_title"
             );
@@ -81,19 +91,19 @@ export default {
         });
       }
       return iklan;
-    }
+    },
   },
   props: ["id"],
   components: {
     Hooper,
     Slide,
-    HooperProgress
+    HooperProgress,
   },
   methods: {
     onStick(data) {
       console.log(data);
     },
-    truncate: function(text, length, suffix) {
+    truncate: function (text, length, suffix) {
       return text.substring(0, length) + suffix;
     },
     moment(arg) {
@@ -104,9 +114,9 @@ export default {
       axios
         .post(urlBase.urlWeb + "/master/berita", {
           type: "BeritaById",
-          id: this.id
+          id: this.id,
         })
-        .then(r => {
+        .then((r) => {
           this.berita = r.data[0];
         });
     },
@@ -114,9 +124,9 @@ export default {
       axios
         .post(urlBase.urlWeb + "/master/berita", {
           type: "AddViewer",
-          id: this.id
+          id: this.id,
         })
-        .then(r => {
+        .then((r) => {
           console.log(r.data);
         });
     },
@@ -124,12 +134,12 @@ export default {
       axios
         .post(urlBase.urlWeb + "/master/iklan", {
           type: "iklanByDevice",
-          web: "true"
+          web: "true",
         })
-        .then(r => {
+        .then((r) => {
           this.iklan = r.data;
         });
-    }
-  }
+    },
+  },
 };
 </script>
